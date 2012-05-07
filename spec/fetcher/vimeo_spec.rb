df2d6fda382fd8df087f82b963b76b9b69f3e72f
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-07 13:07:53 -0400

    Basic spec outline for how the Vimeo fetcher is supposed to work

---
+++ spec/fetcher/vimeo_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+module Fetcher
+  describe Vimeo do
+    describe "#fetch" do
+      it "returns data about the most recent video"
+      it "sets the data about the most recent video to reader :data"
+    end
+  end
+end
