70eae518ea5821c71b6c58c803b14bb05328e7ec
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-15 14:14:59 -0400

    [#29503675] Improve spec test message, add API key.

--- spec/fetcher/map_quest_traffic_spec.rb
+++ spec/fetcher/map_quest_traffic_spec.rb
@@ -4,6 +4,8 @@ module Fetcher
   describe MapQuestTraffic do
     describe "#fetch" do
       before(:each) do
+        MapQuestTraffic.API_Key = "arandomkey"
+
         location_url = ""
         stub_request(:get, location_url).to_return(body: nil)
 
@@ -16,7 +18,7 @@ module Fetcher
         ["22207", 1],
         [21201, 5],
       ].each do |zip_code, count|
-        it "returns a count of #{count} severe traffic incidents"
+        it "#{zip_code} returns a count of #{count} severe traffic incidents"
       end
     end
   end
