73ae4de52cf4552f421f0b079d7229352d5b94d1
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-15 07:56:23 -0400

    [#29503629] Remove duplication in UrlStatus spec.

--- spec/fetcher/url_status_spec.rb
+++ spec/fetcher/url_status_spec.rb
@@ -5,50 +5,36 @@ module Fetcher
     ANY_VALID_URL = "http://www.bing.com"
     let( :urlstatus ) { UrlStatus.new ANY_VALID_URL }
 
-    context "Successful Request" do
-      [
-        HTTP_OK,
-        HTTP_CREATED,
-        HTTP_ACCEPTED,
-        HTTP_NON_AUTHORITATIVE,
-        HTTP_NO_CONTENT,
-        HTTP_RESET_CONTENT,
-      ].each do |code, message|
-        it "return { available: true } on #{code}" do
-          stub_request(:get, ANY_VALID_URL).to_return(status: [code, message])
+    [
+      [HTTP_OK, true],
+      [HTTP_CREATED, true],
+      [HTTP_ACCEPTED, true],
+      [HTTP_NON_AUTHORITATIVE, true],
+      [HTTP_NO_CONTENT, true],
+      [HTTP_RESET_CONTENT, true],
+      [HTTP_BAD_REQUEST, false],
+      [HTTP_FORBIDDEN, false],
+      [HTTP_INTERNAL_SERVER_ERROR, false],
+    ].each do |status, expected_result|
+      it "return { available: true } on #{status}" do
+        stub_request(:get, ANY_VALID_URL).to_return(status: status)
 
-          urlstatus.fetch.should == { available: true }
-          urlstatus.data.should  == { available: true }
-        end
+        urlstatus.fetch.should == { available: expected_result }
+        urlstatus.data.should  == { available: expected_result }
       end
     end
 
-    context "Unsuccessful Request" do
-      [
-        HTTP_BAD_REQUEST,
-        HTTP_FORBIDDEN,
-        HTTP_INTERNAL_SERVER_ERROR,
-      ].each do |code, message|
-        it "return { available: false } on #{code}" do
-          stub_request(:get, ANY_VALID_URL).to_return(status: [code, message])
+    [
+      HTTParty::RedirectionTooDeep,
+      HTTParty::ResponseError,
+      SocketError,
+      Timeout::Error
+    ].each do |exception|
+      it "return { available: false } on exception #{exception}" do
+        stub_request(:get, ANY_VALID_URL).to_raise(exception)
 
-          urlstatus.fetch.should == { available: false }
-          urlstatus.data.should  == { available: false }
-        end
-      end
-
-      [
-        HTTParty::RedirectionTooDeep,
-        HTTParty::ResponseError,
-        SocketError,
-        Timeout::Error
-      ].each do |exception|
-        it "return { available: false } on exception #{exception}" do
-          stub_request(:get, ANY_VALID_URL).to_raise(exception)
-
-          urlstatus.fetch.should == { available: false }
-          urlstatus.data.should  == { available: false }
-        end
+        urlstatus.fetch.should == { available: false }
+        urlstatus.data.should  == { available: false }
       end
     end
   end
