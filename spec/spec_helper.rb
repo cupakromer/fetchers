54e9f75dd9eff2aee668fa752a4d20494ce99e8b
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-20 15:19:25 -0400

    Update helper with BingMapsTraffic

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,5 +5,6 @@ require 'fetcher/vimeo'
 require 'fetcher/etsy'
 require 'fetcher/url_status'
 require 'fetcher/map_quest_traffic'
+require 'fetcher/bing_maps_traffic'
 
 require_relative 'spec_fetcher_helper'
