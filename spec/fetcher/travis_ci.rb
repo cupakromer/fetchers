172e529790fb420e34aec5de9edb8357d305ff78
Aaron Kromer <aaron.kromer@gmail.com>
2012-05-21 01:31:48 -0400

    [#29503695] Basic test case data and spec.

---
+++ spec/fetcher/travis_ci.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+require_relative 'test_data/travis_ci'
+
+module Fetcher
+  describe TravisCI do
+    describe "#fetch" do
+      before( :each ) do
+        url = "http://travis-ci.org/owner_name/name/builds.json"
+
+        stub_request(:get, url).to_return(body: TestData::TravisCI::JSON_DATA)
+      end
+
+      let( :travis_ci_project ) {
+        Project = Struct.new :owner_name, :name
+        TravisCI.new Project.new("owner_name", "name")
+      }
+
+      it "returns data about the most recent build" do
+        travis_ci_project.fetch.should == TestData::TravisCI::EXPECTED_DATA
+      end
+
+      it "sets the data about the most recent build to reader :data" do
+        travis_ci_project.fetch
+        travis_ci_project.data.should == TestData::TravisCI::EXPECTED_DATA
+      end
+
+    end
+  end
+end
